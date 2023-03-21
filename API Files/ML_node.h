28b73bf9eca0a724e030a934e4f7c687edc26713
Carmen Thom <thomrya22@outlook.com>
2023-03-21 17:22:40 -0500

    Comments for Gaussian

--- API Files/ML_node.h
+++ API Files/ML_node.h
@@ -285,30 +285,19 @@ double ML_node::ML_SiLU_derv(double input)
     return ((1+exp(-input))+(input*exp(-input)))/ pow(1+exp(-input),2);
 }
 
-
+/// @brief The Gaussian activation function
+/// @param inputs input <double>: the sum of the weights, inputs, and biases as a double
+/// @return The evaluation of the gaussian function w.r.t its inputs
 double ML_node::ML_Gaussian_func(double input)
 {
-    /*
-        args:
-            input <double>: the sum of the weights, inputs, and biases as a double
-
-        Use:
-            The identity activation function
-    
-    */
    return exp(pow(input, 2) * -1);
 
 }
 
+/// @brief The Gaussian activation derivitive function
+/// @param inputs input <double>: the sum of the weights, inputs, and biases as a double
+/// @return The derivative of the gaussian function w.r.t its inputs
 double ML_node::ML_Gaussian_derv(double input)
 {
-    /*
-        args:
-            input <double>: Error Terms
-
-        Use:
-            The identity activation derivitive function
-    
-    */
     return -2*input*exp(pow(input, 2) * -1);
 }
