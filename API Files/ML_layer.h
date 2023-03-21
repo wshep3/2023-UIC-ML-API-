92b86b149183796c850292b8cf47e4f82e897157
Carmen Thom <thomrya22@outlook.com>
2023-03-21 16:58:55 -0500

    Solve layer

--- API Files/ML_layer.h
+++ API Files/ML_layer.h
@@ -15,7 +15,7 @@ class ML_layer {
     private:
         std::vector<ML_node> node_layer;
         ML_nType layer_type;
-        
+        std::vector<double> Solve_layer(std::vector<double>);
 
 };
 
@@ -29,4 +29,12 @@ ML_layer::ML_layer(ML_nType ntype, int layer_size, int size_previous)
         this->node_layer.push_back(temp);
     }
     this->layer_type = ntype;
+}
+
+std::vector<double> ML_layer::Solve_layer(std::vector<double> inputs){
+    std::vector<double> outputs;
+    for (int i = 0; i < node_layer.size(); i++){
+        outputs.push_back(node_layer[i].solve_node(inputs));
+    }
+    return outputs;
 }
