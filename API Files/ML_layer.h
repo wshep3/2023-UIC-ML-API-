ed024d4b8bb00c886d01fb5a22af890895cc9d2c
wshep3 <126131553+wshep3@users.noreply.github.com>
2023-03-21 15:26:22 -0500

    layer constructor

--- API Files/ML_layer.h
+++ API Files/ML_layer.h
@@ -9,18 +9,24 @@
 class ML_layer {
 
     public:
-        ML_layer(ML_nType activation_type, int layer_size);
+        ML_layer(ML_nType, int, int);
 
 
     private:
-        std::vector<ML_layer> node_layer;
+        std::vector<ML_node> node_layer;
+        ML_nType layer_type;
 
 
 };
 
 
 
-ML_layer::ML_layer(ML_nType ntype, int size)
+ML_layer::ML_layer(ML_nType ntype, int layer_size, int size_previous)
 {
-    
+    for(int i=0;i<layer_size;i++)
+    {
+        ML_node temp = ML_node(ntype, size_previous);
+        this->node_layer.push_back(temp);
+    }
+    this->layer_type = ntype;
 }
