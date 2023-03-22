d49b0bed195261eb717e170d6ee913a99f73607e
wshep3 <126131553+wshep3@users.noreply.github.com>
2023-03-22 17:49:17 -0500

    defining more ML_network methods

--- API Files/ML_network.h
+++ API Files/ML_network.h
@@ -4,10 +4,43 @@
 #include <math.h>
 #include "ML_node.h"
 #include "ML_layer.h"
+
+enum ML_eType 
+{
+    NON_Declared,
+    MSE,
+
+};
+
+
 class ML_network
 {
     public:
-        ML_network(ML_nType, int);
+        ML_network(int, ML_eType);
+        std::vector<double> solve(std::vector<double>);
+        void train(std::vector<std::vector<double>>, std::vector<std::vector<double>>, int, bool);
+
     private:
-         std::vector<ML_layer> layer_layer;
+        ML_eType network_error;
+        std::vector<ML_layer> layer_layer;
+        int input_size;
 };
+
+
+
+ML_network::ML_network(int inputs, ML_eType error_type)
+{
+    this->input_size = inputs;
+    this->network_error = error_type;
+}
+
+
+std::vector<double> ML_network::solve(std::vector<double> x_data)
+{
+    return {};
+}
+
+void ML_network::train(std::vector<std::vector<double>> x_data, std::vector<std::vector<double>> y_data, int epochs, bool trace)
+{
+    return;
+}
