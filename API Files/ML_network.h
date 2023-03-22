963fd53729cac138829589a2b042c3380373e266
Carmen Thom <thomrya22@outlook.com>
2023-03-22 10:38:20 -0500

    ML_network frame

--- API Files/ML_network.h
+++ API Files/ML_network.h
@@ -1,1 +1,13 @@
 // API created by William Shepelak and Carmen Thom 3/19/2023
+
+#include <vector>
+#include <math.h>
+#include "ML_node.h"
+#include "ML_layer.h"
+class ML_network
+{
+    public:
+        ML_network(ML_nType, int);
+    private:
+         std::vector<ML_layer> layer_layer;
+};
